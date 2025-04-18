5bcead6b7e358ae24a1a9b0289d7449921d8cfb3
Matheus Jesus <matheussantos930@gmail.com>
2025-04-18 10:58:47 -0300

    primeiroCommit

---
+++ src/main/java/com/matheusjsg/SisDeCadastro/SisDeCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.matheusjsg.SisDeCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisDeCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisDeCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusjsg/SisDeCadastro/SisDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusjsg.SisDeCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
